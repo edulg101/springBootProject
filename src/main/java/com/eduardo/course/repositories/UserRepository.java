8f4e74ef773ecf37821dd409bece6053c992eef6
Eduardo Gerhardt <edulg101@yahoo.com.br>
2020-06-22 17:14:15 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/eduardo/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.eduardo.course.repositories;
+
+import com.eduardo.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User,Long>{
+
+}
