b31ed7e36f68d616152db832aa3bafabcd2218c4
Eduardo Gerhardt <edulg101@yahoo.com.br>
2020-06-23 16:02:52 -0300

    Product

---
+++ src/main/java/com/eduardo/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.eduardo.course.repositories;
+
+import com.eduardo.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product,Long>{
+
+}
