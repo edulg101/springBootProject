ddc1d31c0c3c46f3b8186658415205caf9ab6364
Eduardo Gerhardt <edulg101@yahoo.com.br>
2020-06-24 11:09:26 -0300

    OrderItem, many-to-many association with extra attributes Part II

--- src/main/java/com/eduardo/course/entities/OrderItem.java
+++ src/main/java/com/eduardo/course/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.eduardo.course.entities;
 import com.eduardo.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -10,7 +11,7 @@ import java.util.Objects;
 public class OrderItem implements Serializable {
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
     private Integer quantity;
     private Double price;
 
@@ -23,6 +24,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder(){
         return id.getOrder();
     }
---
+++ src/main/java/com/eduardo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.eduardo.course.repositories;
+
+import com.eduardo.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository <OrderItem,Long>{
+
+}
