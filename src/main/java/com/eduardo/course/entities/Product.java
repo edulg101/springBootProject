c23743c9981a5e93c67728f00fcfbcbfddda6f85
Eduardo Gerhardt <edulg101@yahoo.com.br>
2020-06-24 10:00:39 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/eduardo/course/entities/Product.java
+++ src/main/java/com/eduardo/course/entities/Product.java
@@ -1,5 +1,7 @@
 package com.eduardo.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -18,10 +20,14 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product(){
+    public Product() {
 
     }
 
--- src/main/java/com/eduardo/course/repositories/CategoryRepository.java
+++ src/main/java/com/eduardo/course/repositories/CategoryRepository.java
@@ -3,6 +3,6 @@ package com.eduardo.course.repositories;
 import com.eduardo.course.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository <Category,Long>{
+public interface CategoryRepository extends JpaRepository <Category, Long>{
 
 }
